8735659101d560905894b84e89eced114374fc78
Ran Liu <xabsox@gmail.com>
2024-07-01 11:43:00 -0700

    Exact enumeration is much faster than monte carlo

--- test/test_ompeval.cpp
+++ test/test_ompeval.cpp
@@ -5,42 +5,37 @@
 
 TEST(test_ompeval, test_equity)
 {
-    omp::EquityCalculator eq;
-    eq.start({"AsKs", "QhQd"});
-    eq.wait();
+    for (int c = 0; c < 10; c++)
+    {
+        omp::EquityCalculator eq;
+        eq.start({"AsKs", "QhQd"}, 0, 0, true);
+        eq.wait();
 
-    auto r = eq.getResults();
-    std::cout << r.equity[0] << " " << r.equity[1] << std::endl;
-
-    eq.start({"AsKs", "8h8d"});
-    eq.wait();
-
-    auto s = eq.getResults();
-    std::cout << s.equity[0] << " " << s.equity[1] << std::endl;
+        auto r = eq.getResults();
+        std::cout << r.equity[0] << " " << r.equity[1] << std::endl;
+    }
 }
 
 TEST(test_ompeval, test_multiprocess)
 {
+    std::vector<omp::EquityCalculator *> eq;
 
-    omp::EquityCalculator eq;
-    eq.start({"AsKs", "QhQd"});
+    for (int c = 0; c < 10; c++)
+    {
+        eq.push_back(new omp::EquityCalculator());
+        eq[c]->start({"AsKs", "QhQd"}, 0, 0, true);
+    }
 
-    omp::EquityCalculator eq2;
-    eq2.start({"AsKs", "8h8d"});
+    for (int c = 0; c < 10; c++)
+    {
+        eq[c]->wait();
+    }
 
-    omp::EquityCalculator eq3;
-    eq3.start({"AcKc", "2h2d"});
+    for (int c = 0; c < 10; c++)
+    {
+        auto r = eq[c]->getResults();
+        std::cout << r.equity[0] << " " << r.equity[1] << std::endl;
 
-    eq.wait();
-    eq2.wait();
-    eq3.wait();
-
-    auto r = eq.getResults();
-    std::cout << r.equity[0] << " " << r.equity[1] << std::endl;
-
-    auto s = eq2.getResults();
-    std::cout << s.equity[0] << " " << s.equity[1] << std::endl;
-
-    auto t = eq3.getResults();
-    std::cout << t.equity[0] << " " << t.equity[1] << std::endl;
+        delete eq[c];
+    }
 }
